5e93291b26cb3545df72629f4094340148420fe8
Alessio Burgio <alessioburgio85@gmail.com>
2020-07-30 21:01:31 +0200

    Updates

---
+++ precache-manifest.69365882269359f5840057cb0d0cdd0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0198e855ae4603e303a1b12e8d1898",
+    "url": "/instagram-clone/index.html"
+  },
+  {
+    "revision": "66e48ae728e37503b56f",
+    "url": "/instagram-clone/static/css/main.5690661b.chunk.css"
+  },
+  {
+    "revision": "3026a9552a23dd1be774",
+    "url": "/instagram-clone/static/js/2.b6004264.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/instagram-clone/static/js/2.b6004264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66e48ae728e37503b56f",
+    "url": "/instagram-clone/static/js/main.e24bae18.chunk.js"
+  },
+  {
+    "revision": "3700a5c3edbaabd3f081",
+    "url": "/instagram-clone/static/js/runtime-main.b83be197.js"
+  }
+]);
